1c0473e88edfee32cd4c0bdd41d89afeb837bcd2
Marc Tanti <marctanti@gmail.com>
2025-03-21 13:26:11 +0100

    updated text_tools langmod to 0.2.0: unk tokens not used in model and probability displayed with adaptive precision

--- teaching/text_tools/langmod/ngrams.py
+++ teaching/text_tools/langmod/ngrams.py
@@ -5,7 +5,7 @@ import nltk
 
 
 ns = [1, 2, 3]
-min_freq = 3
+min_freq = 2
 
 all_tokens = re.split(
     '[^a-z]+',
@@ -19,10 +19,12 @@ all_sents = [
     for sent in nltk.corpus.brown.sents()
 ]
 
+token_freqs = collections.Counter(all_tokens)
+
 vocab = ['EDGE', 'UNK'] + [
     token
     for (token, freq) in sorted(
-        collections.Counter(all_tokens).items(),
+        token_freqs.items(),
         key=(lambda item: item[1]),
         reverse=True
     )
@@ -45,12 +47,17 @@ for sent in all_sents:
         for (i, ngram) in enumerate(nltk.ngrams(sent_indexes, n)):
             if n == 1 and i == 0:
                 continue # First edge token is only there for use in prefixes.
-            prefix = ','.join(str(index) for index in ngram[:-1])
+            prefix = ngram[:-1]
             target = ngram[-1]
-            ngrams[n][prefix][target] += 1
+            if unk_index in prefix or target == unk_index:
+                continue
+            prefix_s = ','.join(str(index) for index in prefix)
+            ngrams[n][prefix_s][target] += 1
 
 
 with open('data.js', 'w', encoding='utf-8') as f:
+    print('const $edgeIndex = ' + json.dumps(edge_index) + ';', file=f)
+    print('const $unkIndex = ' + json.dumps(unk_index) + ';', file=f)
     print('const $vocab = ' + json.dumps(vocab).replace(' ', '') + ';', file=f)
     print('const $tokenToIndex = ' + json.dumps({t: i for (i, t) in enumerate(vocab)}).replace(' ', '') + ';', file=f)
     print('const $ngrams = ' + json.dumps(ngrams).replace(' ', '') + ';', file=f)
